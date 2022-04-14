abc5eef6c14f1082dcfef7e2cf033e193671f3ba
Ítalo Fernandes <italo.avelino@outlook.com.br>
2022-04-14 19:56:04 -0300

    Iniciando API REST padronizada e escalavel

---
+++ api/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const config = require("config");
+const roteador = require("./rotas/fornecedores/index");
+
+app.use(express.json());
+
+app.use("/api/fornecedores", roteador);
+
+app.listen(config.get("api.porta"), () => console.log("A API está funcionando!"));
